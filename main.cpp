f357ac16c7c84f788d6459561cd057f57d4f318c
Mikkel C <mikch10@gmail.com>
2023-08-30 23:11:38 +0200

    small fix so code actually runs

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 
 int main() {
     std::cout << "We cannot do anything yet ..." << std::endl;
-
     MonkeyToken x("Hi", "peter");
-
-    std::cout << x.p << std::endl;
+    std::cout << bob << " " << x.literal << " " << x.type << std::endl;
     return 0;
 }
