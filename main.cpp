7ef2b0e29d2c46a3059d227b704ea48f7a5e133a
Mikkel C <mikch10@gmail.com>
2023-08-31 02:03:23 +0200

    created mapping between string and enum type for tokens

--- main.cpp
+++ main.cpp
@@ -4,7 +4,5 @@
 
 int main() {
     std::cout << "We cannot do anything yet ..." << std::endl;
-    MonkeyToken x("Hi", "peter");
-    std::cout << bob << " " << x.literal << " " << x.type << std::endl;
     return 0;
 }
--- types/monkey-token.h
+++ types/monkey-token.h
@@ -7,14 +7,87 @@
 
 #ifndef MONKEYLANG_MONKEY_TOKEN_H
 #define MONKEYLANG_MONKEY_TOKEN_H
+enum MonkeyTokenType {
+    UNKNOWN,
+    END,
+    IDENT,
+    INT,
+    ASSIGN,
+    PLUS,
+    COMMA,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    FUNC,
+    LET
+};
 
 struct MonkeyToken {
     std::string literal;
-    std::string type;
+    MonkeyTokenType tokenType;
 
-    MonkeyToken(std::string literal, std::string type) : literal(literal), type(type) {}
+    MonkeyToken(std::string lit, MonkeyTokenType type) :
+            literal(lit),
+            tokenType(type) {}
+
+    static std::string getSymbol(MonkeyTokenType token) {
+        switch (token) {
+            case UNKNOWN:
+                return "UNKNOWN";
+            case END:
+                return "EOF";
+            case IDENT:
+                return "IDENT";
+            case INT:
+                return "INT";
+            case ASSIGN:
+                return "=";
+            case PLUS:
+                return "+";
+            case COMMA:
+                return ",";
+            case SEMICOLON:
+                return ";";
+            case LPAREN:
+                return "(";
+            case RPAREN:
+                return ")";
+            case LBRACE:
+                return "{";
+            case RBRACE:
+                return "}";
+            case FUNC:
+                return "FUNCTION";
+            case LET:
+                return "LET";
+            default:
+                return "INVALID";
+        }
+    }
+
+    static MonkeyTokenType getType(std::string &symbol) {
+        if (symbol == "UNKNOWN") return UNKNOWN;
+        if (symbol == "EOF") return END;
+        if (symbol == "IDENT") return IDENT;
+        if (symbol == "INT") return INT;
+        if (symbol == "=") return ASSIGN;
+        if (symbol == "+") return PLUS;
+        if (symbol == ",") return COMMA;
+        if (symbol == ";") return SEMICOLON;
+        if (symbol == "(") return LPAREN;
+        if (symbol == ")") return RPAREN;
+        if (symbol == "{") return LBRACE;
+        if (symbol == "}") return RBRACE;
+        if (symbol == "FUNCTION") return FUNC;
+        if (symbol == "LET") return LET;
+
+        return UNKNOWN; // Default case
+    }
 };
 
-const std::string bob = "BOB";
+static std::string getSymbol(MonkeyTokenType token);
+
 
 #endif //MONKEYLANG_MONKEY_TOKEN_H
